7db162249e8c777abb31905164e9f523867ba176
lSylra <esgallegoa@unal.edu.co>
2026-05-27 11:14:09 -0500

    Intento 3

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -14,12 +14,12 @@ def test_01():
 
     #
     # Retorna error si la carpeta output/ no existe
-    if not os.path.exists("files.csv"):
-        raise FileNotFoundError("File 'files.csv' not found")
+    if not os.path.exists("files/countries.csv"):
+        raise FileNotFoundError("File 'files/countries.csv' not found")
 
     #
     # Lee el contenido del archivo output.txt
-    dataframe = pd.read_csv("files.csv")
+    dataframe = pd.read_csv("files/countries.csv")
     dataframe = dataframe.set_index("countries")
 
     assert dataframe["count"]["United States of America"] == 579
